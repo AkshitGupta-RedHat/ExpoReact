51c8c5845da125cdb63876e85d1bb3a6d7923afb
Akshit-RedHat <akshitguptasmart@gmail.com>
2020-05-20 00:01:28 -0400

    Adding  functionality to add list item in the app

--- App.js
+++ App.js
@@ -1,35 +1,44 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
-  // const [inputText, SetInputText] = useState("Text Changed");
+  const [enteredGoal, setInputText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enterText) => {
+    setInputText(enterText)
+  }
+
+  const addGoalHandler = () => {
+
+    setCourseGoals([...courseGoals, 
+      {key:Math.random().toString,value: enteredGoal}]);
+    console.log("text addedd", courseGoals);
+  }
+
   return (
-    <View style={ styles.screen}>
-      <View style={{}}><Text>hell</Text>
+    <View>
+      <View>
+        <TextInput placeholder="Course Goals" style={styles.TextInputStyle} onChangeText={goalInputHandler} />
+        <Button title="Add" onPress={addGoalHandler} />
       </View>
-      <View style={{ width: 50, height: 50, backgroundColor: 'green',flex:1 }}>
-      </View>
-      <View style={{ width: 50, height: 50, backgroundColor: 'yellow' }}>
-      </View>
-      <View style={{justifyContent: 'space-between', alignItem: 'center' }}>
-        <TextInput placeholder="Course Goals" style={{ width: '80%', borderBottomColor: 'black', borderBottomWidth: 1, padding: 10 }} />
-        <Button title="Add" />
-      </View>
+      <FlatList keyExtractor={(item, index) => item.id} data={courseGoals} renderItem={itemData => (  
+        <View style={styles.listStyle}>
+          <Text>{itemData.item.value}</Text>
+        </View>)} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen:{
-    padding: 50,width:"100%",height:300,flexDirection:"row",justifyContent:'space-around',alignItems:"center" 
+  TextInputStyle: {
+    marginLeft: 30, width: '80%', borderBottomColor: 'black', borderBottomWidth: 1, paddingTop: 40
   },
-  red:{
-    backgroundColor: 'red'
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  listStyle: {
+    padding: 10,
+    marginLeft: 30,
+    color: 'red',
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
